ca56cc5385c5361f92fc794f04dbf8565a1a46d5
TheMarionette <the.freaking.marionette@gmail.com>
2018-04-07 22:11:28 +0200

    Revert "griffin: Enforce RRO on framework-res"
    This reverts commit c34a276eaf228c6fc12c5635e7fbd2978962b92a since it broke the boot process.

--- aosp_griffin.mk
+++ aosp_griffin.mk
@@ -26,11 +26,7 @@ $(call inherit-product, vendor/aosp/common.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Overlays
 DEVICE_PACKAGE_OVERLAYS += device/motorola/griffin/overlay
-
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2560
